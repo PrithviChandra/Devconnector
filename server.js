cbad3e09c51e5465e83ffbfa2884ffaca59b9b88
Prithvi <prithvichandra.mec@gmail.com>
2021-05-09 00:03:34 +0530

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express= require('express');
+const connectDB=require("./config/db");
+
 const app= express();
 
+//Connect to database
+connectDB();
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
 
 
 app.get('/', (req,res)=>res.send("API started running"));
